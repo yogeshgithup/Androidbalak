69880eec029b93f9f8994ece548dd515755d425d
vidhigithub <vdchandnani15@gmail.com>
2019-03-24 10:26:32 +0530

    change in timetable class

--- app/src/main/java/nl/psdcompany/duonavigationdrawer/example/Timetable.java
+++ app/src/main/java/nl/psdcompany/duonavigationdrawer/example/Timetable.java
@@ -2,20 +2,39 @@ package nl.psdcompany.duonavigationdrawer.example;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
 
 public class Timetable extends Fragment {
 
+
     @Nullable
     @Override
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //returning our layout file
         //change R.layout.yourlayoutfilename for each of your fragments
